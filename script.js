1852692d8dd23dd892d8578fe83df8b12afb648e
colemyhre <colemyhre6@gmail.com>
2024-03-17 12:29:12 -0700

    Add event listeners to number buttons and clear buttons, make them change the value in the display

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+let currentValue = 0;
+let displayValue = 8;
+
+
+
 function add(num1, num2) {
     return num1+num2;
 }
@@ -6,10 +12,35 @@ function subtract(num1, num2) {
     return num1-num2;
 }
 
-fuction multiply(num1, num2) {
+function multiply(num1, num2) {
     return num1*num2;
 }
 
 function divide(num1, num2) {
     return num1/num2;
 }
+
+let display = document.querySelector('.display');
+
+let numbers = document.querySelector('.numbers');
+for (let i=0; i<10; i++) {
+    let digit = document.createElement('button');
+            digit.classList.add('number');
+            digit.textContent = i;
+            
+            digit.addEventListener('click', () => {
+                displayValue=digit.textContent;
+                console.log(displayValue);
+                display.textContent = displayValue;
+            })
+            numbers.appendChild(digit);
+}
+
+let clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    displayValue=0;
+    currentValue=0;
+    display.textContent = '';
+})
+
+
